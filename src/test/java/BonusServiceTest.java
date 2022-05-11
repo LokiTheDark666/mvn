ec647a435536d38b21ef0bf8a24c2ecd5b0fcec0
LokiTheDark666 <khcr.loki@mail.ru>
2022-05-11 17:04:26 +0300

    patch

--- src/test/java/BonusServiceTest.java
+++ src/test/java/BonusServiceTest.java
@@ -23,7 +23,7 @@ public class BonusServiceTest {
         BonusService service = new BonusService();
 
         // подготавливаем данные:
-        long amount = 1_000_000_60;
+        long amount = 1_000_050_55;
         boolean registered = true;
         long expected = 500;
 
@@ -35,29 +35,13 @@ public class BonusServiceTest {
     }
 
     @org.junit.jupiter.api.Test
-    void shouldCalculateForRegisteredAndUnderLimit() {
+    void shouldCalculateForRegisteredAndZeroLimit() {
         BonusService service = new BonusService();
 
         // подготавливаем данные:
-        long amount = 1000_60;
-        boolean registered = false;
-        long expected = 30;
-
-        // вызываем целевой метод:
-        long actual = service.calculate(amount, registered);
-
-        // производим проверку (сравниваем ожидаемый и фактический):
-        assertEquals(expected, actual);
-    }
-
-    @org.junit.jupiter.api.Test
-    void shouldCalculateForRegisteredAndUnderLimit() {
-        BonusService service = new BonusService();
-
-        // подготавливаем данные:
-        long amount = 1000_600;
+        long amount = 1060;
         boolean registered = true;
-        long expected = 30;
+        long expected = 0;
 
         // вызываем целевой метод:
         long actual = service.calculate(amount, registered);
